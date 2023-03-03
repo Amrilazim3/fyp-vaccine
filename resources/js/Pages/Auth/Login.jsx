0d1af8f578597cfe911074681c6e87ee8e27b572
Amril Azim <a.azim0711@gmail.com>
2023-03-03 23:36:02 +0800

    fix class attribute

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -47,7 +47,7 @@ export default function Login({ status, canResetPassword }) {
             )}
 
             <form onSubmit={submit}>
-                <h3 class="font-semibold text-xl mb-4 text-gray-800">
+                <h3 className="font-semibold text-xl mb-4 text-gray-800">
                     Login Back To Your Account
                 </h3>
 
@@ -124,12 +124,12 @@ export default function Login({ status, canResetPassword }) {
                     </span>
                 </div>
 
-                <div class="relative py-6">
-                    <div class="absolute inset-0 flex items-center">
-                        <div class="w-full border-t border-gray-400" />
+                <div className="relative py-6">
+                    <div className="absolute inset-0 flex items-center">
+                        <div className="w-full border-t border-gray-400" />
                     </div>
-                    <div class="relative flex justify-center text-sm">
-                        <span class="px-2 bg-white text-gray-800">
+                    <div className="relative flex justify-center text-sm">
+                        <span className="px-2 bg-white text-gray-800">
                             Or continue with
                         </span>
                     </div>
