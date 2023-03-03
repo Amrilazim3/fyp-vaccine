1e6ed7ee80293c978fbfe84c1f5e94f51194504b
Amril Azim <a.azim0711@gmail.com>
2023-03-03 12:58:59 +0800

    create Feature component

---
+++ resources/js/Components/Feature.jsx
@@ -0,0 +1,166 @@
+export default function Feature() {
+    return (
+        <div className="pb-16" style={{ fontFamily: '"Lato", sans-serif' }}>
+            <section className="max-w-8xl mx-auto container bg-white py-16">
+                <div>
+                    <div
+                        role="contentinfo"
+                        className="flex items-center flex-col px-4"
+                    >
+                        <p
+                            tabIndex={0}
+                            className="focus:outline-none uppercase text-sm text-center text-gray-600 leading-4"
+                        >
+                            in few easy steps
+                        </p>
+                        <hh1
+                            tabIndex={0}
+                            className="focus:outline-none text-4xl lg:text-4xl font-extrabold text-center leading-10 text-gray-800 lg:w-5/12 md:w-9/12 pt-4"
+                        >
+                            You Can Start Tracking Your Child Vaccination!
+                        </hh1>
+                    </div>
+                    <div
+                        tabIndex={0}
+                        aria-label="group of cards"
+                        className="focus:outline-none mt-20 flex flex-wrap justify-center gap-10 px-4"
+                    >
+                        <div
+                            tabIndex={0}
+                            aria-label="card 1"
+                            className="focus:outline-none flex sm:w-full md:w-5/12 pb-20"
+                        >
+                            <div className="w-20 h-20 relative mr-5">
+                                <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                                <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                    <img
+                                        src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG1.svg"
+                                        alt="drawer"
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-10/12">
+                                <h2
+                                    tabIndex={0}
+                                    className="focus:outline-none text-lg font-bold leading-tight text-gray-800"
+                                >
+                                    Create an account
+                                </h2>
+                                <p
+                                    tabIndex={0}
+                                    className="focus:outline-none text-base text-gray-600 leading-normal pt-2"
+                                >
+                                    It provides a very simple start, no need to
+                                    write a lot of code, you just import it and
+                                    start the primitive components and create
+                                    the ones you need.
+                                </p>
+                            </div>
+                        </div>
+                        <div
+                            tabIndex={0}
+                            aria-label="card 2"
+                            className="focus:outline-none flex sm:w-full md:w-5/12 pb-20"
+                        >
+                            <div className="w-20 h-20 relative mr-5">
+                                <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                                <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                    <img
+                                        src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG2.svg"
+                                        alt="check"
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-10/12">
+                                <h2
+                                    tabIndex={0}
+                                    className="focus:outline-none text-lg font-semibold leading-tight text-gray-800"
+                                >
+                                    Add your child's information
+                                </h2>
+                                <p
+                                    tabIndex={0}
+                                    className="focus:outline-none text-base text-gray-600 leading-normal pt-2"
+                                >
+                                    Modify the visual appearance of your site –
+                                    including colors, fonts, margins and other
+                                    style-related properties – with a
+                                    sophisticated style.
+                                </p>
+                            </div>
+                        </div>
+                        <div
+                            tabIndex={0}
+                            aria-label="card 3"
+                            className="focus:outline-none flex sm:w-full md:w-5/12 pb-20"
+                        >
+                            <div className="w-20 h-20 relative mr-5">
+                                <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                                <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                    <img
+                                        src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG3.svg"
+                                        alt="html tag"
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-10/12">
+                                <h2
+                                    tabIndex={0}
+                                    className="focus:outline-none text-lg font-semibold leading-tight text-gray-800"
+                                >
+                                    View child schedule
+                                </h2>
+                                <p
+                                    tabIndex={0}
+                                    className="focus:outline-none text-base text-gray-600 leading-normal pt-2"
+                                >
+                                    Instead of just giving you the tools to
+                                    create your own site, they offer you a list
+                                    of themes you can choose from. Thus a handy
+                                    product.
+                                </p>
+                            </div>
+                        </div>
+                        <div
+                            tabIndex={0}
+                            aria-label="card 4"
+                            className="focus:outline-none flex sm:w-full md:w-5/12 pb-20"
+                        >
+                            <div className="w-20 h-20 relative mr-5">
+                                <div className="absolute top-0 right-0 bg-indigo-100 rounded w-16 h-16 mt-2 mr-1" />
+                                <div className="absolute text-white bottom-0 left-0 bg-indigo-700 rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                                    <img
+                                        src="https://tuk-cdn.s3.amazonaws.com/can-uploader/icon_and_text-SVG4.svg"
+                                        alt="monitor"
+                                    />
+                                </div>
+                            </div>
+                            <div className="w-10/12">
+                                <h2
+                                    tabIndex={0}
+                                    className="focus:outline-none text-lg font-semibold leading-tight text-gray-800"
+                                >
+                                    Get notify for every date!
+                                </h2>
+                                <p
+                                    tabIndex={0}
+                                    className="focus:outline-none text-base text-gray-600 leading-normal pt-2"
+                                >
+                                    We have chosen the bright color palettes
+                                    that arouse the only positive emotions. The
+                                    kit that simply assures to be loved by
+                                    everyone.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-center items-center">
+                    <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 lg:text-xl lg:font-bold  rounded text-white px-4 sm:px-10 border border-indigo-700 py-2 sm:py-4 text-sm">
+                        Get Started
+                    </button>
+                </div>
+            </section>
+        </div>
+    );
+}
