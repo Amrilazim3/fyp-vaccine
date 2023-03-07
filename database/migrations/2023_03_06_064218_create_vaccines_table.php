75b8534e2bc3fa7c719482b891a544d154d65ea1
Amril Azim <a.azim0711@gmail.com>
2023-03-07 10:15:56 +0800

    add column description

--- database/migrations/2023_03_06_064218_create_vaccines_table.php
+++ database/migrations/2023_03_06_064218_create_vaccines_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('vaccines', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('description');
             $table->unsignedInteger('max_to_taken');
             $table->timestamps();
         });
