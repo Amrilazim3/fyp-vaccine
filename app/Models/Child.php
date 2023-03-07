f6e6911fd0f1a6313a662e4b07afa4866a78b0f9
Amril Azim <a.azim0711@gmail.com>
2023-03-07 17:20:13 +0800

    add relationship

--- app/Models/Child.php
+++ app/Models/Child.php
@@ -10,4 +10,16 @@ class Child extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function parent()
+    {
+        return $this->belongsTo(User::class, 'parent_id', 'id');
+    }
+
+    public function vaccines()
+    {
+        return $this->belongsToMany(Vaccine::class)
+            ->using(ChildVaccine::class)
+            ->withPivot('dose_taken');
+    }
 }
