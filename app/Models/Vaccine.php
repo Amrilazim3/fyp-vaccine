7e30f23b907be88e12daac91621b26fd9c7445f4
Amril Azim <a.azim0711@gmail.com>
2023-03-09 16:51:52 +0800

    add relatioship

--- app/Models/Vaccine.php
+++ app/Models/Vaccine.php
@@ -11,10 +11,25 @@ class Vaccine extends Model
 
     protected $guarded = [];
 
+    public $_reqs = [];
+
     public function children()
     {
         return $this->belongsToMany(Child::class)
             ->using(ChildVaccine::class)
             ->withPivot('dose_taken');
+    }
+
+    public function requirementsByMonth()
+    {
+        return $this->hasMany(VaccineRequirement::class, 'vaccine_id')
+            ->where('type', 'month')
+            ->orderBy('value');
+    }
+
+    public function requirementsNotByMonth()
+    {
+        return $this->hasMany(VaccineRequirement::class, 'vaccine_id')
+            ->where('type', '!=', 'month');
     }
 }
