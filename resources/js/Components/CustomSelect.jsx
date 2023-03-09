b9b1778640f78099aa1a033aeb643ee7a518d1d0
Amril Azim <a.azim0711@gmail.com>
2023-03-09 21:16:46 +0800

    create custom select component

---
+++ resources/js/Components/CustomSelect.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const CustomSelect = ({ options, onChange, className = '', isRequired = false, ...props }) => {
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleChange = (e) => {
+    setSelectedValue(e.target.value);
+
+    onChange(e.target.value);
+  };
+
+  return (
+    <div>
+      <select
+        {...props}
+        value={selectedValue}
+        onChange={handleChange}
+        className={
+            'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
+            className
+        }
+        required={isRequired}
+      >
+        <option key={0} value="">select {props.id}</option>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default CustomSelect;
