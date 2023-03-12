4fae9f60495a8d3739fbb03b4bb11dde8d253b82
Amril Azim <a.azim0711@gmail.com>
2023-03-12 23:01:52 +0800

    display dynamic data from back-end

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,32 +1,16 @@
+import ChildrenForm from "@/Components/ChildrenForm";
 import ContentContainer from "@/Components/ContentContainer";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
+import { useState } from "react";
 
-const children = [
-    {
-        id: 1,
-        name: "Sarah",
-        age: 3,
-        completedPercentage: 80,
-        nextVaccination: "April 2023",
-    },
-    {
-        id: 2,
-        name: "John",
-        age: 5,
-        completedPercentage: 60,
-        nextVaccination: "June 2023",
-    },
-    {
-        id: 3,
-        name: "Emily",
-        age: 2,
-        completedPercentage: 90,
-        nextVaccination: "May 2023",
-    },
-];
+export default function Dashboard({ children, ...props }) {
+    const [isOpenChildrenForm, setIsOpenChildrenForm] = useState(false);
 
-export default function Dashboard(props) {
+    const handleOnClickAddChildrenButtton = () => {
+        setIsOpenChildrenForm(!isOpenChildrenForm);
+    };
+
     return (
         <AuthenticatedLayout
             auth={props.auth}
@@ -40,71 +24,95 @@ export default function Dashboard(props) {
             <Head title="Dashboard" />
 
             <ContentContainer>
-                {/* if user doesn't register any children */}
-                {/* <div className="flex flex-col justify-center p-6 items-center h-96">
-                    <h1 className="text-2xl font-bold text-gray-900 mb-8 text-center">
-                        You don't have any children registered yet!
-                    </h1>
-                    <a
-                        href="/register"
-                        className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded"
-                    >
-                        Register your child now!
-                    </a>
-                </div> */}
-                {/* if user already has children registered */}
-                <div>
-                    <h1 className="text-gray-800 text-xl md:text-2xl font-bold mb-4 md:mb-6">
-                        Vaccination Summary
-                    </h1>
-                    <ul className="bg-gray-100 p-4 md:p-8 lg:p-12 rounded-lg">
-                        {children.map((child, index) => (
-                            <li
-                                key={child.id}
-                                className="mb-4 md:mb-8 lg:mb-12"
-                            >
-                                <div className="text-indigo-500 text-sm">
-                                    Child #{index + 1}
-                                </div>
-                                <div>
-                                    <div className="flex justify-between">
-                                        <div className="flex flex-col">
-                                            <h2 className="text-lg lg:text-xl font-medium text-gray-900">
-                                                {child.name}
-                                            </h2>
-                                            <p className="text-gray-600">
-                                                {child.age} years old
-                                            </p>
-                                        </div>
-                                        <button className="text-sm font-semibold rounded-md px-5 text-white bg-indigo-500 hover:bg-indigo-400">
-                                            view
-                                        </button>
+                {children.length == 0 && (
+                    <div className="flex flex-col justify-center p-6 items-center h-96">
+                        <h1 className="text-2xl font-bold text-gray-900 mb-8 text-center">
+                            You don't have any children registered yet!
+                        </h1>
+                        <button
+                            type="button"
+                            onClick={handleOnClickAddChildrenButtton}
+                            className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded"
+                        >
+                            Register your child now!
+                        </button>
+                    </div>
+                )}
+
+                {children.length > 0 && (
+                    <div>
+                        <h1 className="text-gray-800 text-xl md:text-2xl font-bold mb-4 md:mb-6">
+                            Vaccination Summary
+                        </h1>
+                        <ul className="bg-gray-100 p-4 md:p-8 lg:p-12 rounded-lg">
+                            {children.map((child, index) => (
+                                <li
+                                    key={child.id}
+                                    className="mb-4 md:mb-8 lg:mb-12"
+                                >
+                                    <div className="text-indigo-500 text-sm">
+                                        Child #{index + 1}
                                     </div>
-                                    <div className="md:flex md:mt-4 md:space-x-10">
-                                        <div className="mt-4 md:mt-0">
-                                            <p className="text-gray-600">
-                                                Completed percentage:
-                                            </p>
-                                            <p className="text-gray-900 font-medium">
-                                                {child.completedPercentage}%
-                                            </p>
+                                    <div>
+                                        <div className="flex justify-between">
+                                            <div className="flex flex-col">
+                                                <h2 className="text-lg lg:text-xl font-medium text-gray-900">
+                                                    {child.name}
+                                                </h2>
+                                                <p className="text-gray-600">
+                                                    {child.age_in_months <=
+                                                    12
+                                                        ? Math.floor(child.age_in_months) + " months old"
+                                                        : Math.floor(child.age_in_months /
+                                                              12) +
+                                                          " years old"}
+                                                </p>
+                                            </div>
+                                            <button className="text-sm font-semibold rounded-md px-5 text-white bg-indigo-500 hover:bg-indigo-400">
+                                                <Link
+                                                    href={
+                                                        "children/" + child.id
+                                                    }
+                                                >
+                                                    view
+                                                </Link>
+                                            </button>
                                         </div>
-                                        <div className="mt-4 md:mt-0">
-                                            <p className="text-gray-600">
-                                                Next vaccination:
-                                            </p>
-                                            <p className="text-gray-900 font-medium">
-                                                {child.nextVaccination}
-                                            </p>
+                                        <div className="md:flex md:mt-4 md:space-x-10">
+                                            <div className="mt-4 md:mt-0">
+                                                <p className="text-gray-600">
+                                                    Percentage Completed:
+                                                </p>
+                                                <p className="text-gray-900 font-medium">
+                                                    {child.percentage_completed}
+                                                </p>
+                                            </div>
+                                            <div className="mt-4 md:mt-0">
+                                                <p className="text-gray-600">
+                                                    Next vaccination:
+                                                </p>
+                                                <p className="text-gray-900 font-medium">
+                                                    {child.next_vaccination_info.name} ({child.next_vaccination_info.date})
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <hr className="border-gray-300 my-4 md:my-8 lg:my-12" />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                                    <hr className="border-gray-300 my-4 md:my-8 lg:my-12" />
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
             </ContentContainer>
+
+            {isOpenChildrenForm && (
+                <ChildrenForm
+                    isOpen={isOpenChildrenForm}
+                    handleOnClose={
+                        handleOnClickAddChildrenButtton
+                    }
+                />
+            )}
         </AuthenticatedLayout>
     );
 }
