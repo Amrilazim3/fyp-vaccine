4965a3ec8509b1300d4d3044a98f0c6c270976dd
Amril Azim <a.azim0711@gmail.com>
2023-03-12 12:29:36 +0800

    extract logic from ScheduleForVaccination job

--- app/Models/VaccineRequirement.php
+++ app/Models/VaccineRequirement.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class VaccineRequirement extends Model
 {
@@ -29,5 +30,31 @@ class VaccineRequirement extends Model
     public function scopeMonth($query)
     {
         return $query->where('type', 'month');
+    }
+
+    public function getVaccinesRequirements()
+    {
+        return Cache::rememberForever('vaccines-requirements', function () {
+            $vaccines = Vaccine::all();
+            $requirements = VaccineRequirement::with(['childrenNotMonth', 'parent'])
+                ->month()
+                ->orderBy('value', 'asc')
+                ->get();
+
+            $flatten = [];
+            $requirements->sortBy('value')->each(function ($req) use ($vaccines, &$flatten) {
+                $vaccine = $vaccines->firstWhere('id', $req->vaccine_id);
+
+                $flatten[] = [
+                    'name' => $vaccine->name,
+                    'model' => $req,
+                    'month' => $req->value,
+                    'depend_on' => !$req->parent ? null : $req->parent->toArray(),
+                    'addtional_reqs' => $req->childrenNotMonth->toArray()
+                ];
+            });
+
+            return $flatten;
+        });
     }
 }
